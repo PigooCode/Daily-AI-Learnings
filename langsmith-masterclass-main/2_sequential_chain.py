dd495ca5094df42a9d5485296b772e14ea08efef
PigooCode <nk110700@gmail.com>
2026-04-15 20:52:40 +0530

    AI learnings 15th April

--- langsmith-masterclass-main/2_sequential_chain.py
+++ langsmith-masterclass-main/2_sequential_chain.py
@@ -2,6 +2,9 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os
+
+os.environ['langchain_project'] = 'llm deq app'
 
 load_dotenv()
 
@@ -15,12 +18,22 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-model = ChatOpenAI()
+model = ChatOpenAI( temperature=0.9, model='gpt-3.5-turbo' )
+
+model2 = ChatOpenAI( temperature=0.5, model='gpt-4o' )
 
 parser = StrOutputParser()
 
-chain = prompt1 | model | parser | prompt2 | model | parser
+chain = prompt1 | model | parser | prompt2 | model2 | parser
 
-result = chain.invoke({'topic': 'Unemployment in India'})
+config = {
+    'tags': ['sequential chain', 'demo', 'Nikhil'],
+    'metadata': {
+        'model1': 'gpt-3.5',
+        'author': 'Nikhil',
+    }
+}
+
+result = chain.invoke({'topic': 'Unemployment in India'},config=config)
 
 print(result)
